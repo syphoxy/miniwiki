31397d3c5ff3cfe278958cc4116a676db9c445f1
Dario Russo <dario@dariorusso.ca>
2017-04-13 20:42:49 -0400

    Refactor structure

--- server.rb
+++ server.rb
@@ -4,61 +4,48 @@ require 'wikicloth'
 require 'json'
 require 'uri'
 
-wiki_logo = 'logo.png'
-wiki_name = 'Wiki'
-use_identicon = false
-css = true
-js = true
-noindex = false
-base_url = ''
-custom_path = ''
-upload_dest = 'uploads'
-url_path_display = nil
-mathjax = false
-mathjax_config = nil
-allow_uploads = true
-allow_editing = true
+before do
+  @config = {
+    wiki_logo: 'logo.png',
+    wiki_name: 'Wiki',
+    use_identicon: false,
+    css: true,
+    js: true,
+    noindex: false,
+    base_url: '',
+    custom_path: '',
+    upload_dest: 'uploads',
+    mathjax: false,
+    mathjax_config: nil,
+    allow_uploads: true,
+    allow_editing: true,
+  }
 
-stylesheets = [
-  {media: 'all',   href: base_url + '/css/gollum.css'},
-  {media: 'all',   href: base_url + '/css/editor.css'},
-  {media: 'all',   href: base_url + '/css/dialog.css'},
-  {media: 'all',   href: base_url + '/css/template.css'},
-  {media: 'print', href: base_url + '/css/print.css'},
-  {media: 'all',   href: 'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.css'},
-]
+  @stylesheets = [
+    {media: 'all', href: @config[:base_url] + '/css/gollum.css'},
+    {media: 'all', href: @config[:base_url] + '/css/editor.css'},
+    {media: 'all', href: @config[:base_url] + '/css/dialog.css'},
+    {media: 'all', href: @config[:base_url] + '/css/template.css'},
+    {media: 'print', href: @config[:base_url] + '/css/print.css'},
+    {media: 'all', href: 'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.css'},
+  ]
 
-stylesheets << {media: 'all', href: custom_path + '/custom.css'} if css
+  @stylesheets << {media: 'all', href: @config[:custom_path] + '/custom.css'} if @config[:css]
 
-scripts = [
-  base_url + '/javascript/jquery-1.7.2.min.js',
-  base_url + '/javascript/mousetrap.min.js',
-  base_url + '/javascript/gollum.js',
-  base_url + '/javascript/gollum.dialog.js',
-  base_url + '/javascript/gollum.placeholder.js',
-  base_url + '/javascript/editor/gollum.editor.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.js',
-]
+  @scripts = [
+    @config[:base_url] + '/javascript/jquery-1.7.2.min.js',
+    @config[:base_url] + '/javascript/mousetrap.min.js',
+    @config[:base_url] + '/javascript/gollum.js',
+    @config[:base_url] + '/javascript/gollum.dialog.js',
+    @config[:base_url] + '/javascript/gollum.placeholder.js',
+    @config[:base_url] + '/javascript/editor/gollum.editor.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.js',
+  ]
 
-scripts << base_url + '/javascript/identicon_canvas.js' if use_identicon
-scripts << base_url + '/' + mathjax_config if mathjax and ! mathjax_config.nil?
-scripts << base_url + '/custom.js' if js
-
-template_data = {
-  wiki_logo: wiki_logo,
-  wiki_name: wiki_name,
-  stylesheets: stylesheets,
-  scripts: scripts,
-  base_url: base_url,
-  noindex: noindex,
-  upload_dest: upload_dest,
-  url_path_display: url_path_display,
-  use_identicon: use_identicon,
-  mathjax: mathjax,
-  mathjax_config: mathjax_config,
-  allow_uploads: allow_uploads,
-  allow_editing: allow_editing,
-}
+  @scripts << @config[:base_url] + '/javascript/identicon_canvas.js' if @config[:use_identicon]
+  @scripts << @config[:base_url] + '/' + @config[:mathjax_config] if @config[:mathjax] and ! @config[:mathjax_config].nil?
+  @scripts << @config[:base_url] + '/custom.js' if @config[:js]
+end
 
 get '/create/:path' do
   'Create: '+ params['path']
@@ -69,10 +56,17 @@ get '/edit/:path' do
 end
 
 get '/:path' do
-  template_data[:editable] = true
-  template_data[:page_exists] = true
-  template_data[:page_title] = 'Hello World!'
-  haml :index, locals: template_data
+  haml :index, locals: {
+    config: @config,
+    page: {
+      stylesheets: @stylesheets,
+      scripts: @scripts,
+      editable: true,
+      exists: true,
+      title: params['path'],
+      body: mediawiki('= ' + params['path'] + ' =')
+    }
+  }
 end
 
 get '/' do
